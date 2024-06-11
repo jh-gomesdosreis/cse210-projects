90dcdb752eae203e75e09a6b49f0001411f39bc2
jh-gomesdosreis <rei21005@byui.edu>
2024-06-11 16:02:28 -0300

    activity 2 complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What's your grade percentage?");
+        string gradePercentage = Console.ReadLine();
+        int grade = int.Parse(gradePercentage);
+
+        string gradeLetter = "";
+
+        if (grade >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (grade >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (grade >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (grade >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else if (grade >= 50)
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {gradeLetter}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        if (grade < 70)
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
